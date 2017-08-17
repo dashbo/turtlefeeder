5684017fd3062294f5b72da9cd2b19309c4e3cd7
Darian Oliphant <darian@darianoliphant.com>
2017-08-16 23:15:39 -0600

    Update turtle_feeder.py
    Got Tweet to post

--- turtle_feeder.py
+++ turtle_feeder.py
@@ -3,6 +3,7 @@
 import picamera
 import os
 import RPi.GPIO as GPIO
+from twython import Twython
 from time import sleep, localtime, time
 
 def feedTurtle():
@@ -65,13 +66,22 @@ def feedTurtleExtra():
 
 if __name__ == '__main__':
 
+    APP_KEY = 'your app key'
+    APP_SECRET = 'your secret app key'
+    OAUTH_TOKEN = 'your oauth token'
+    OAUTH_TOKEN_SECRET = 'your secret oauth token'
+    
     meals = 1
+    twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    auth = twitter.get_authentication_tokens()
+    
     camera = picamera.PiCamera()
-    camera.resolution = (1920, 1080)
+    camera.resolution = (640, 480)
     while True:
         if (localtime().tm_hour % 1, localtime().tm_min % 1, localtime().tm_sec % 5) == (0, 0, 0):
             timestamp = str(localtime().tm_hour) + str(localtime().tm_min) + str(localtime().tm_sec)
-            print('Turtle feed day and time: ' + str(localtime().tm_mday) + '/' + str(localtime().tm_mon) + '/' + str(localtime().tm_year) + ' ' + str(localtime().tm_hour) + ':' + str(localtime().tm_min) + ':' + str(localtime().tm_sec) + '.')
+            twStatus = 'Turtle feed day and time: ' + str(localtime().tm_mday) + '/' + str(localtime().tm_mon) + '/' + str(localtime().tm_year) + ' ' + str(localtime().tm_hour) + ':' + str(localtime().tm_min) + ':' + str(localtime().tm_sec) + '.'
+            print(twStatus)
             print('Number of meals is ' + str(meals) + '.')
             vidName = 'ttlfeed_' + timestamp
             camera.start_recording(vidName + '.h264')
@@ -89,4 +99,7 @@ if __name__ == '__main__':
             camera.wait_recording(3)
             camera.stop_recording()
             os.system('MP4Box -add ' + vidName + '.h264 ' + vidName + '.mp4')
-            
+
+            video = open(vidName + '.mp4', 'rb')
+            response = twitter.upload_video(media=video, media_type='video/mp4')
+            twitter.update_status(status=twStatus, media_ids=[response['media_id']])
