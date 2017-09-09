661d29e0d09d6dcdecf560493dbf09de54659c1f
Darian Oliphant <darian@darianoliphant.com>
2017-09-09 10:26:03 -0600

    improved timestamp,  cron every 10 minutes or so

--- turtle_feeder_1.py
+++ turtle_feeder_1.py
@@ -63,43 +63,62 @@ def feedTurtleExtra():
 
     GPIO.cleanup()
 
+def timeSinceLast():
+    lastfeeding = open('lastturtlefeeding.txt', 'r')
+    timeSince = int(time()) - int(lastfeeding.read())
+    lastfeeding.close()
+    return timeSince
 
+def recordTime():
+    feedingTime = open('lastturtlefeeding.txt', 'w')
+    feedingTime.write(str(int(time())))
+    feedingTime.close()
+    
 if __name__ == '__main__':
 
-    #For the Twitter API
-    APP_KEY = 'your app key'
-    APP_SECRET = 'your secret app key'
-    OAUTH_TOKEN = 'your oauth token'
-    OAUTH_TOKEN_SECRET = 'your secret oauth token'
-    twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-    auth = twitter.get_authentication_tokens()
-    
-    #Set up the camera
-    camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
-    camera.vflip = True
-    camera.hflip = True
+    timeSinceLast = timeSinceLast()
 
-    timestamp = str(localtime().tm_hour) + str(localtime().tm_min) + str(localtime().tm_sec)
-    twStatus = 'Turtle feed day and time: ' + str(localtime().tm_mday) + '/' + str(localtime().tm_mon) + '/' + str(localtime().tm_year) + ' ' + str(localtime().tm_hour) + ':' + str(localtime().tm_min) + ':' + str(localtime().tm_sec) + '.'
-    print(twStatus)
-    vidName = 'ttlfeed_' + timestamp
-    camera.start_recording(vidName + '.h264')
-    camera.wait_recording(1)
-    feedTurtle()
-    camera.wait_recording(.5)
-    feedTurtleBack()
-    camera.wait_recording(.5)
-    feedTurtle()
-    camera.wait_recording(1)
-    camera.stop_recording()
+    #Check if it is time to feed the turtle and feed if it is.
+    if timeSinceLast <= 172800:
+        exit()
 
-    #Convert video to MP4
-    os.system('MP4Box -add ' + vidName + '.h264 ' + vidName + '.mp4')
+    else:
+        recordTime()
 
-    #Tweet video
-    video = open(vidName + '.mp4', 'rb')
-    response = twitter.upload_video(media=video, media_type='video/mp4')
-    twitter.update_status(status=twStatus, media_ids=[response['media_id']])
+        #For the Twitter API
+        APP_KEY = ''
+        APP_SECRET = ''
+        OAUTH_TOKEN = ''
+        OAUTH_TOKEN_SECRET = ''
+        twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+        auth = twitter.get_authentication_tokens()
+        
+        #Set up the camera
+        camera = picamera.PiCamera()
+        camera.resolution = (640, 480)
+        camera.vflip = True
+        camera.hflip = True
 
-    exit()
+        timestamp = str(localtime().tm_hour) + str(localtime().tm_min) + str(localtime().tm_sec)
+        twStatus = 'Turtle feed day and time: ' + str(localtime().tm_mday) + '/' + str(localtime().tm_mon) + '/' + str(localtime().tm_year) + ' ' + str(localtime().tm_hour) + ':' + str(localtime().tm_min) + ':' + str(localtime().tm_sec) + '.'
+        print(twStatus)
+        vidName = 'ttlfeed_' + timestamp
+        camera.start_recording(vidName + '.h264')
+        camera.wait_recording(1)
+        feedTurtle()
+        camera.wait_recording(.5)
+        feedTurtleBack()
+        camera.wait_recording(.5)
+        feedTurtle()
+        camera.wait_recording(1)
+        camera.stop_recording()
+
+        #Convert video to MP4
+        os.system('MP4Box -add ' + vidName + '.h264 ' + vidName + '.mp4')
+
+        #Tweet video
+        video = open(vidName + '.mp4', 'rb')
+        response = twitter.upload_video(media=video, media_type='video/mp4')
+        twitter.update_status(status=twStatus, media_ids=[response['media_id']])
+
+        exit()
